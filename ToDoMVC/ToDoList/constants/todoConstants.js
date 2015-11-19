555dc1c8cf68a6e28a6f10032e8f27a26c2ec732
csoulioti <christina5906@yahoo.gr>
2015-11-19 22:45:37 +0200

    Add todo constants

---
+++ ToDoMVC/ToDoList/constants/todoConstants.js
@@ -0,0 +1,11 @@
+﻿import keyMirror from 'keymirror';
+
+module.exports = keyMirror({
+    TODO_CREATE: null,
+    TODO_COMPLETE: null,
+    TODO_DESTROY: null,
+    TODO_DESTROY_COMPLETED: null,
+    TODO_TOGGLE_COMPLETE_ALL: null,
+    TODO_UNDO_COMPLETE: null,
+    TODO_UPDATE_TEXT: null
+});
