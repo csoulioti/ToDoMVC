68f358fc5e0fec93ebcb5649448823e1830ce544
csoulioti <christina5906@yahoo.gr>
2015-11-19 22:46:39 +0200

    Update client.js

--- ToDoMVC/Scripts/build/client.js
+++ ToDoMVC/Scripts/build/client.js
@@ -1,6 +1,6 @@
 ﻿// This is the entry point for the bundle file that is used during server side creation of the HTML pages
 // It is used by the Reactjs.NET engine to produce the HTML markup server side and is a stripped down version of
 // the corresponding client bundle as in the server side we need only the widgets and all their dependencies
-import TodoItem from 'expose?TodoItem!../../ToDoList/components/todoItem';
+import TodoApp from 'expose?TodoApp!../../ToDoList/components/TodoApp';
 import React from 'expose?React!react';
 import ReactDOM from 'expose?ReactDOM!react-dom';
