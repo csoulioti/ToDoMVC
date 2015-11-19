3fc0d93dd8f18d07fbd4c4d4b9d4205e950a6634
csoulioti <christina5906@yahoo.gr>
2015-11-19 22:44:03 +0200

    Add todo app components

---
+++ ToDoMVC/ToDoList/components/todoApp.jsx
@@ -0,0 +1,56 @@
+﻿var Footer = require('./footer');
+var Header = require('./header');
+var MainSection = require('./mainSection');
+var React = require('react');
+var TodoStore = require('../stores/todoStore');
+
+/**
+ * Retrieve the current TODO data from the TodoStore
+ */
+function getTodoState() {
+    return {
+        allTodos: TodoStore.getAll(),
+        areAllComplete: TodoStore.areAllComplete()
+    };
+}
+
+var TodoApp = React.createClass({
+
+    getInitialState: function() {
+        return getTodoState();
+    },
+
+    componentDidMount: function() {
+        TodoStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+        TodoStore.removeChangeListener(this._onChange);
+    },
+
+    /**
+     * @return {object}
+     */
+    render: function() {
+        return (
+          <div>
+            <Header />
+            <MainSection
+              allTodos={this.state.allTodos}
+          areAllComplete={this.state.areAllComplete}
+        />
+        <Footer allTodos={this.state.allTodos} />
+      </div>
+    );
+},
+
+/**
+ * Event handler for 'change' events coming from the TodoStore
+ */
+_onChange: function() {
+    this.setState(getTodoState());
+}
+
+});
+
+module.exports = TodoApp;
