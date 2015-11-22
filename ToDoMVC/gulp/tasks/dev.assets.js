d0e3021357529247eda415bf9e61504985e7538c
csoulioti <christina5906@yahoo.gr>
2015-11-22 09:57:51 +0200

    Improve gulp tasks

--- ToDoMVC/gulp/tasks/dev.assets.js
+++ ToDoMVC/gulp/tasks/dev.assets.js
@@ -2,7 +2,6 @@
 var webpack = require('webpack');
 var config = require('../../webpack.config');
 var gutil = require('gulp-util');
-var ProgressBar = require('progress');
 
 gulp.task('dev-assets', function (callback) {
 	//Webpack development configuration
@@ -10,18 +9,6 @@ gulp.task('dev-assets', function (callback) {
 	myDevConfig.output.filename = '[name].bundle.js';
 	myDevConfig.debug = true;
 	myDevConfig.devtool = 'inline-source-map';
-	myDevConfig.plugins = [
-      new webpack.ProgressPlugin(function (percentage) {
-      	var bar = new ProgressBar('Compiling |:bar| :percent', {
-      		complete: '\u001b[42m \u001b[0m',
-      		incomplete: ' ',
-      		width: 100,
-      		total: 100
-      	});
-      	var value = percentage * 100;
-      	bar.tick(value);
-      })
-	];
 	// Run webpack
 	webpack(myDevConfig, function (err, stats) {
 		//Log webpack errors to the console
